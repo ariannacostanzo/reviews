722e12852006d5eb47132e766b6f59b931b9431f
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-15 19:21:27 +0100

    adding array of objects

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 const logSomething = (something) => console.log(something);
 
+const users = [
+    {
+        name : 'Martha Watson',
+        role: 'Web developer',
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nisi rerum iusto quasi nobis ipsam, eveniet architecto sapiente asperiores, explicabo dolorem, repudiandae porro! Sit, deleniti reprehenderit rerum et ipsam praesentium!',
+        image: 'persona1.jpg'
+    },
+    {
+        name : 'Gerard White',
+        role: 'IA developer',
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nisi rerum iusto quasi nobis ipsam, eveniet architecto sapiente asperiores, explicabo dolorem, repudiandae porro! Sit, deleniti reprehenderit rerum et ipsam praesentium!',
+        image: 'persona2.avif'
+    },
+    {
+        name : 'Anthony West',
+        role: 'Teacher',
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nisi rerum iusto quasi nobis ipsam, eveniet architecto sapiente asperiores, explicabo dolorem, repudiandae porro! Sit, deleniti reprehenderit rerum et ipsam praesentium!',
+        image: 'persona3.jpg'
+    },
+    {
+        name : 'Annalise Pratt',
+        role: 'Web designer',
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nisi rerum iusto quasi nobis ipsam, eveniet architecto sapiente asperiores, explicabo dolorem, repudiandae porro! Sit, deleniti reprehenderit rerum et ipsam praesentium!',
+        image: 'persona4.jpg'
+    },
+    {
+        name : 'Nell Junior',
+        role: 'Game designer',
+        review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nisi rerum iusto quasi nobis ipsam, eveniet architecto sapiente asperiores, explicabo dolorem, repudiandae porro! Sit, deleniti reprehenderit rerum et ipsam praesentium!',
+        image: 'persona5.jpg'
+    },
+]
